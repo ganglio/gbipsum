6cd4d8318ac3722e05c8bcc52c0fc0afa8b59217
Roberto Torella <robertot@square-enix.com>
2013-02-01 15:12:19 +0000

    Finished module implementation

--- test/gbipsum.test.js
+++ test/gbipsum.test.js
@@ -2,6 +2,20 @@ var	vows = require("vows"),
     assert = require("assert"),
     gbipsum = require("../lib/gbipsum");
 
-vows.describe("Testing gbipsum").addBatch({
-
+vows.describe("gbipsum").addBatch({
+	'Testing': {
+		topic: function(){ return gbipsum },
+		'words': function(topic) {
+			var out = topic.words(5);
+			assert.equal(out.split(" ").length,5);
+		},
+		'sentences': function(topic) {
+			var out = topic.sentences(5);
+			assert.equal(out.split("\n").length,5);
+		},
+		'paragraphs': function (topic) {
+			var out = topic.paragraphs(5,3);
+			assert.equal(out.split("\n\n").length,5);
+		}
+	}
 }).export(module);
